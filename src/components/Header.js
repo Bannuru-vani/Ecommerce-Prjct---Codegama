08aaf6d33b932c969415d46cb84192d645f8fe22
Bannuru-vani <vanibannuru11@gmail.com>
2025-03-28 07:30:07 +0530

    first comment

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+    <nav>
+      <ul>
+        <li>
+          <li to="/">Home</li>
+        </li>
+        <li>
+          <li to="/cart">Cart</li>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  )
+}
+
+export default Header
